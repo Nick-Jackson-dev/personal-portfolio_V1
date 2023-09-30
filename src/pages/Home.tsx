861b94970ba6320bcca3ba62ce864405d7bd0fe7
Nick Jackson <niceguysgroup@gmail.com>
2023-09-30 12:56:30 -0400

    create SingleColumn layout component and implement on Home page component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,12 @@
+import SingleColumn from "../layouts/SingleColumn"
+
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <SingleColumn className="home-wrapper">
+      <p>Hello</p>
+      <div>These should be</div>
+      <div>These should be</div>
+      <div>Vertically stacked!</div>
+    </SingleColumn>
+  )
 }
